0e81df568e22c1ad6d4a92504cc2b80e5c795014
navyaguthula <navyasree.guttula@gmail.com>
2025-01-31 17:39:53 +0000

    Update variable2.sh

--- variable2.sh
+++ variable2.sh
@@ -3,7 +3,7 @@
 PERSON1=RAHIM
 PERSON2=ROBERT
 
-echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: Hi $PERSON1"
-echo "$PERSON1:: How are you doing!"
-echo "$PERSON2:: I am doing great"
+echo "${PERSON1}:: Hi ${PERSON2}"
+echo "${PERSON2}:: Hi ${PERSON1}"
+echo "${PERSON1}:: How are you doing!"
+echo "${PERSON2}:: I am doing great"
